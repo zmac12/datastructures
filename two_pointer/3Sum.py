436c93dd8e5b92db77cc9e0ef18cdb02fdb64246
Zach <zach.mcquiston35@gmail.com>
2023-03-26 22:24:16 -0600

    Adding 3Sum solution, faster than 90%

---
+++ two_pointer/3Sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+
+        for i, a in enumerate(nums):
+            if a > 0:
+                break
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            j, k = i + 1, len(nums) - 1
+            while j < k:
+                minisum = nums[i] + nums[j] + nums[k]
+                if minisum > 0:
+                    k -= 1
+                elif minisum < 0:
+                    j += 1
+                else:
+                    res.append([a, nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                    while nums[j] == nums[j-1] and j<k:
+                        j+=1
+        return res
